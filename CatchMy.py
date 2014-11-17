6a7fb12c2e9c9786120c53213db8eaddae96a374
ilist <s6837893@mail.zih.tu-dresden.de>
2014-11-17 11:31:54 +0100

    [under development] added rudimentary multi-bus-stop support

--- CatchMy.py
+++ CatchMy.py
@@ -2,18 +2,22 @@ from gi.repository import Gtk
 import fetch_station
 import os
 
-class catchMyPicon:
-
+class catchMyPicon(Gtk.StatusIcon):
     def __init__(self):
         self.statusicon = Gtk.StatusIcon()
         self.statusicon.set_from_file(os.environ['HOME'] + "/.catch-my-bus-python/bus_stop_icon.png")
         self.statusicon.connect("popup-menu", self.right_click_event)
+        self.stop_station = "Helmholzstraße"
 
 
     def right_click_event(self, icon, button, time):
         self.menu = Gtk.Menu()
 
-        content = fetch_station.get_departure_list("Heinrich-Zille-Straße")
+        content = fetch_station.get_departure_list(self.stop_station)
+
+        first_menu_element = Gtk.MenuItem()
+        first_menu_element.set_label("Current Stop: " + self.stop_station)
+        self.menu.append(first_menu_element)
 
         i = 0
         test = ""
@@ -33,10 +37,23 @@ class catchMyPicon:
                 self.menu.append(new_menu_element)
                 test = ""
             i = i + 1    
+
+        stop_one = Gtk.MenuItem()
+        stop_one.set_label("Set Stop: Zellescher Weg")
+
+        stop_one.connect("activate", self.set_stop_one)
+
+        stop_two = Gtk.MenuItem()
+        stop_two.set_label("Set Stop: Helmholzstraße")
+
+        stop_two.connect("activate", self.set_stop_two)
         quit = Gtk.MenuItem()
         quit.set_label("Quit")
         quit.connect("activate", Gtk.main_quit)
         
+        self.menu.append(stop_one)
+        self.menu.append(stop_two)
+
         self.menu.append(quit)
 
         self.menu.show_all()
@@ -46,5 +63,13 @@ class catchMyPicon:
 
         self.menu.popup(None, None, pos, self.statusicon, button, time)
 
+    def set_stop_one(self, widget):
+        self.stop_station = "Zellescher Weg"
+
+    def set_stop_two(self, widget):
+        self.stop_station = "Helmholzstraße"
+
+
+stop_station = "Technische Universität"
 catchMyPicon()
 Gtk.main()
--- fetch_station.py
+++ fetch_station.py
@@ -5,7 +5,6 @@ def get_departure_list(stop_name):
 	stop_name = str(stop_name.encode("ascii", "ignore"))
 	url = "http://widgets.vvo-online.de/abfahrtsmonitor/Abfahrten.do?ort=Dresden&hst=" + stop_name + "&vz="
 	content = urllib.request.urlopen(url).read()
-	#content = "[[\"11\",\"Bhlau\",\"3\"],[\"11\",\"Zschertnitz\",\"6\"],[\"61\",\"Lbtau\",\"20\"],[\"61\",\"E Pohlandplatz\",\"23\"],[\"11\",\"Bhlau\",\"33\"],[\"11\",\"Zschertnitz\",\"36\"],[\"11\",\"Gorbitz\",\"48\"],[\"61\",\"Btf. Gruna\",\"53\"],[\"11\",\"Zschertnitz\",\"66\"],[\"11\",\"Bhlau\",\"73\"]]"
 
 	content = str(content, "utf-8")
 
@@ -13,6 +12,8 @@ def get_departure_list(stop_name):
 	content = content.replace(",", "")
 	content = content.replace("]", "")
 	content = content.split("\"")
+	if content[5] == "":
+		content[5] = "0"
 	while content.count("") > 0:
 		content.remove("")
 
