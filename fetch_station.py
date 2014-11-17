b83bcb45101da87cde3b768cc49ad10522265b3b
JustusAdam <development@justusadam.com>
2014-11-17 15:52:56 +0100

    json parsing

--- fetch_station.py
+++ fetch_station.py
@@ -1,20 +1,20 @@
 import urllib.request
+import json
+import itertools
+
 
 def get_departure_list(stop_name):
-	stop_name = stop_name.replace(" ", "%20")
-	stop_name = str(stop_name.encode("ascii", "ignore"))
-	url = "http://widgets.vvo-online.de/abfahrtsmonitor/Abfahrten.do?ort=Dresden&hst=" + stop_name + "&vz="
-	content = urllib.request.urlopen(url).read()
-	#content = "[[\"11\",\"Bhlau\",\"3\"],[\"11\",\"Zschertnitz\",\"6\"],[\"61\",\"Lbtau\",\"20\"],[\"61\",\"E Pohlandplatz\",\"23\"],[\"11\",\"Bhlau\",\"33\"],[\"11\",\"Zschertnitz\",\"36\"],[\"11\",\"Gorbitz\",\"48\"],[\"61\",\"Btf. Gruna\",\"53\"],[\"11\",\"Zschertnitz\",\"66\"],[\"11\",\"Bhlau\",\"73\"]]"
+    stop_name = stop_name.replace(" ", "%20")
+    stop_name = str(stop_name.encode("ascii", "ignore"))
+    url = "http://widgets.vvo-online.de/abfahrtsmonitor/Abfahrten.do?ort=Dresden&hst=" + stop_name + "&vz="
 
-	content = str(content, "utf-8")
 
-	content = content.replace("[", "")
-	content = content.replace(",", "")
-	content = content.replace("]", "")
-	content = content.split("\"")
-	while content.count("") > 0:
-		content.remove("")
+    file = urllib.request.urlopen(url).read().decode('utf-8')
 
-	return content
+    content = json.loads(file)
 
+    # use this to get an output which is a single list of stuff
+    return list(itertools.chain(*content))
+
+    # return content
+    # use this to get an output that list of lists
