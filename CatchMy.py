becfeabf962ac3d97b865be4b60ba50842e12a20
ilist <s6837893@mail.zih.tu-dresden.de>
2014-11-17 03:20:35 +0100

    Initial commit of the (more or less) working applet

---
+++ CatchMy.py
@@ -0,0 +1,53 @@
+from gi.repository import Gtk
+import httpreq
+import os
+
+class catchMyPicon:
+
+    namelist = ["Hans", "Walter"]
+
+    def __init__(self):
+        self.statusicon = Gtk.StatusIcon()
+        self.statusicon.set_from_file(os.environ['HOME'] + "/bus_stop_icon.png")
+        self.statusicon.connect("popup-menu", self.right_click_event)
+
+
+    def right_click_event(self, icon, button, time):
+        self.menu = Gtk.Menu()
+
+        content = httpreq.get_departure_list()
+
+        i = 0
+        test = ""
+        for x in content:
+	        if i != 2:
+		        test = test + str(x) + " "
+        	else:
+        		hours = int(str(x)) // 60
+        		minutes = int(str(x)) % 60
+        		hours = str(hours)
+        		if minutes < 10:
+        			minutes = "0" + str(minutes)
+        		else:
+        			minutes = str(minutes)
+        			test = test + hours + ":" + minutes
+	        	new_menu_element = Gtk.MenuItem()
+	        	new_menu_element.set_label(test)
+	        	self.menu.append(new_menu_element)
+        		test = ""
+        	i = (i + 1) % 3       
+        quit = Gtk.MenuItem()
+        quit.set_label("Quit")
+        quit.connect("activate", Gtk.main_quit)
+        
+        self.menu.append(quit)
+
+        self.menu.show_all()
+
+        def pos(menu, icon):
+                return (Gtk.StatusIcon.position_menu(menu, icon))
+
+        self.menu.popup(None, None, pos, self.statusicon, button, time)
+
+catchMyPicon()
+Gtk.main()
---
+++ fetch_station.py
@@ -0,0 +1,16 @@
+import urllib.request
+
+def get_departure_list():
+	content = urllib.request.urlopen("http://widgets.vvo-online.de/abfahrtsmonitor/Abfahrten.do?ort=Dresden&hst=Heinrich-Zille-Stra0e&vz=").read()
+	#content = "[[\"11\",\"Bhlau\",\"3\"],[\"11\",\"Zschertnitz\",\"6\"],[\"61\",\"Lbtau\",\"20\"],[\"61\",\"E Pohlandplatz\",\"23\"],[\"11\",\"Bhlau\",\"33\"],[\"11\",\"Zschertnitz\",\"36\"],[\"11\",\"Gorbitz\",\"48\"],[\"61\",\"Btf. Gruna\",\"53\"],[\"11\",\"Zschertnitz\",\"66\"],[\"11\",\"Bhlau\",\"73\"]]"
+
+	content = str(content, "utf-8")
+
+	content = content.replace("[", "")
+	content = content.replace(",", "")
+	content = content.replace("]", "")
+	content = content.split("\"")
+	while content.count("") > 0:
+		content.remove("")
+	return content
+
