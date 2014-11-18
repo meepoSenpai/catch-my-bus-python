949dabe10ef849e7c981941626b75c3fa6dca6d8
ilist <s6837893@mail.zih.tu-dresden.de>
2014-11-18 15:19:13 +0100

    Fixed not being able to fetch stops if the VVO passes '' as the time variable

--- fetch_station.py
+++ fetch_station.py
@@ -6,29 +6,29 @@ import json
 
 
 def get_departure_list(stop_name, city_name):
-    stop_name = stop_name.replace("ä", "ae").replace("ü", "ue").replace("ö", "oe").replace("ß", "ss").replace(" ", "%20")
-    city_name = city_name.replace("ä", "ae").replace("ü", "ue").replace("ö", "oe").replace("ß", "ss").replace(" ", "%20")
-    url = "http://widgets.vvo-online.de/abfahrtsmonitor/Abfahrten.do?ort=" + city_name +"&hst=" + stop_name + "&vz="
+	stop_name = stop_name.replace("ä", "ae").replace("ü", "ue").replace("ö", "oe").replace("ß", "ss").replace(" ", "%20")
+	city_name = city_name.replace("ä", "ae").replace("ü", "ue").replace("ö", "oe").replace("ß", "ss").replace(" ", "%20")
+	url = "http://widgets.vvo-online.de/abfahrtsmonitor/Abfahrten.do?ort=" + city_name +"&hst=" + stop_name + "&vz="
 
 
-    file = urllib.request.urlopen(url).read().decode('utf-8')
+	file = urllib.request.urlopen(url).read().decode('utf-8')
 
-    content = json.loads(file)
+	content = json.loads(file)
 
-    return content
+	return content
 
 def compile_menu(station="Heinrich-Zille-Straße", city_name="Dresden"):
-    return [
-        ' '.join([number, direction, ':'.join(split_time(time))]) for number, direction, time in get_departure_list(station, city_name)
-    ]
+	return [
+		' '.join([number, direction, ':'.join(split_time(time))]) for number, direction, time in get_departure_list(station, city_name)
+	]
 
 def split_time(time):
-    hours = str(int(str(time)) // 60)
-    minutes = str(int(str(time)) % 60)
-    if len(minutes) < 2:
-        minutes = "0" + minutes
-    else:
-        minutes = str(minutes)
-    return hours, minutes
-
-a = compile_menu("Heinrich-Zille-Straße")
+	if time == "":
+		time = 0
+	hours = str(int(str(time)) // 60)
+	minutes = str(int(str(time)) % 60)
+	if len(minutes) < 2:
+		minutes = "0" + minutes
+	else:
+		minutes = str(minutes)
+	return hours, minutes
