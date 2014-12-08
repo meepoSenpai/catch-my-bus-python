2571abbab7c6a2c9e46574ff5984c8ed072fe55f
Kilian Koeltzsch <koeltzsch@gmail.com>
2014-12-08 17:57:50 +0100

    requests instead of urllib, so you don't have to escape german chars

--- fetch_station.py
+++ fetch_station.py
@@ -1,36 +1,27 @@
 __author__ = 'JustusAdam'
 
-import urllib.request
 import json
-
-
-def escape_german(string):
-	for char, escaped in [
-		('ä', 'ae'),
-		('ü', 'ue'),
-		('ö', 'oe'),
-		('ß', 'ss'),
-		(' ', '%20')
-	]:
-		string = string.replace(char, escaped)
-	return string
+import requests
 
 
 def get_departure_list(stop_name, city_name):
-	stop_name = escape_german(stop_name)
-	city_name = escape_german(city_name)
-	url = "http://widgets.vvo-online.de/abfahrtsmonitor/Abfahrten.do?ort=" + city_name +"&hst=" + stop_name + "&vz="
-
 	try:
-		file = urllib.request.urlopen(url).read().decode('utf-8')
-
-		content = json.loads(file)
-	except urllib.error.URLError as e:
-		content= ["No Internet Connection"]
+		r = requests.get(
+		    url='http://widgets.vvo-online.de/abfahrtsmonitor/Abfahrten.do',
+		    params={
+		    	'ort': city_name,
+		    	'hst': stop_name
+		    }
+		)
+		if r.status_code == 200:
+			content = json.loads(r.content)
+		else:
+			content = ['No Internet Connection']
 
 	print(content)
 
 	return content
+
 
 def compile_menu(station="Heinrich-Zille-Straße", city_name="Dresden"):
 	departure_list = get_departure_list(station, city_name)
@@ -40,6 +31,7 @@ def compile_menu(station="Heinrich-Zille-Straße", city_name="Dresden"):
 		return [
 			' '.join([number, direction, ':'.join(split_time(time))]) for number, direction, time in departure_list
 		]
+
 
 def split_time(time):
 	if time == "":
